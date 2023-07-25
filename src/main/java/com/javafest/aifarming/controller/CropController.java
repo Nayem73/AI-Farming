7c414cb61ffb8804b861f46c5d0628fddc6095c2
Nayem73 <nayem73@gmail.com>
2023-07-25 16:16:25 +0600

    Added Controllers for exposing rest api's Alhamdulillah. Service class is not doing anything now.

---
+++ src/main/java/com/javafest/aifarming/controller/CropController.java
@@ -0,0 +1,53 @@
+package com.javafest.aifarming.controller;
+
+import com.javafest.aifarming.model.Crop;
+import com.javafest.aifarming.repository.CropRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/crops")
+public class CropController {
+
+    private final CropRepository cropRepository;
+
+    @Autowired
+    public CropController(CropRepository cropRepository) {
+        this.cropRepository = cropRepository;
+    }
+
+    @GetMapping
+    public List<Crop> getAllCrops() {
+        return cropRepository.findAll();
+    }
+
+    @GetMapping("/{disease}")
+    public List<Crop> getCropByDisease(@PathVariable String disease) {
+        return cropRepository.findByDisease(disease);
+    }
+
+    @PostMapping
+    public Crop addCrop(@RequestBody Crop crop) {
+        return cropRepository.save(crop);
+    }
+
+    @PutMapping("/{id}")
+    public Crop updateCrop(@PathVariable Long id, @RequestBody Crop updatedCrop) {
+        Crop crop = cropRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid Crop ID: " + id));
+
+        crop.setDisease(updatedCrop.getDisease());
+        crop.setMarkdownFile(updatedCrop.getMarkdownFile());
+        return cropRepository.save(crop);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCrop(@PathVariable Long id) {
+        Crop crop = cropRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid Crop ID: " + id));
+
+        cropRepository.delete(crop);
+    }
+}
--- src/main/java/com/javafest/aifarming/repository/CropRepository.java
+++ src/main/java/com/javafest/aifarming/repository/CropRepository.java
@@ -2,8 +2,13 @@ package com.javafest.aifarming.repository;
 
 import com.javafest.aifarming.model.Crop;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CropRepository extends JpaRepository<Crop, Long> {
+    @Query("SELECT c FROM Crop c WHERE c.disease = ?1")
+    List<Crop> findByDisease(String disease);
 }
