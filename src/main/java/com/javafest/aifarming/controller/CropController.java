276f617835d2c36926cccebcf26c995b4793961c
Nayem73 <nayem73@gmail.com>
2023-07-26 12:27:22 +0600

    Alhamdulillah. Handled duplicate for crop entry where cropname and crop disease is same. learnt lot from this: crop.getCropCategory().getId() crop.getCropCategory().getTitle()

--- src/main/java/com/javafest/aifarming/controller/CropController.java
+++ src/main/java/com/javafest/aifarming/controller/CropController.java
@@ -1,6 +1,8 @@
 package com.javafest.aifarming.controller;
 
 import com.javafest.aifarming.model.Crop;
+import com.javafest.aifarming.model.CropCategory;
+import com.javafest.aifarming.repository.CropCategoryRepository;
 import com.javafest.aifarming.repository.CropRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -13,10 +15,12 @@ import java.util.List;
 public class CropController {
 
     private final CropRepository cropRepository;
+    private final CropCategoryRepository cropCategoryRepository;
 
     @Autowired
-    public CropController(CropRepository cropRepository) {
+    public CropController(CropRepository cropRepository, CropCategoryRepository cropCategoryRepository) {
         this.cropRepository = cropRepository;
+        this.cropCategoryRepository = cropCategoryRepository;
     }
 
     @GetMapping("/")
@@ -24,32 +28,48 @@ public class CropController {
         return cropRepository.findAll();
     }
 
-//    @GetMapping()
-//    public List<Crop> getCropsByDisease(@RequestParam String disease) {
-//        return cropRepository.findByDisease(disease);
-//    }
-@GetMapping
-public List<Crop> getCropsByDisease(@RequestParam(value = "title", required = false) String categoryTitle,
-                                    @RequestParam(value = "disease", required = false) String disease) {
-    List<Crop> crops;
-    if (categoryTitle != null && disease != null) {
-        crops = cropRepository.findByCategoryTitleAndDisease(categoryTitle, disease);
-    } else if (categoryTitle != null) {
-        crops = cropRepository.findByDisease(categoryTitle);
-    } else if (disease != null) {
-        crops = cropRepository.findByDisease(disease);
-    } else {
-        // Handle the case when both parameters are missing.
-        // For example, return an error message or an empty list.
-        crops = Collections.emptyList();
+    @GetMapping
+    public List<Crop> getCropsByDisease(@RequestParam(value = "crop", required = false) String categoryTitle,
+                                        @RequestParam(value = "disease", required = false) String disease,
+                                        @RequestParam(value = "search", required = false) String search) {
+        List<Crop> crops;
+        if (categoryTitle != null && search != null) {
+            crops = cropRepository.findByCategoryTitleAndDisease(categoryTitle, search);
+        } else if (categoryTitle != null) {
+            crops = cropRepository.findByTitle(categoryTitle);
+        } else if (disease != null) {
+            crops = cropRepository.findByDisease(disease);
+        } else if (search != null) {
+            crops = cropRepository.findBySearch(search);
+        }
+        else {
+            // Handle the case when both parameters are missing.
+            // For example, return an error message or an empty list.
+            crops = Collections.emptyList();
+        }
+        return crops;
     }
-    return crops;
-}
 
     @PostMapping
     public Crop addCrop(@RequestBody Crop crop) {
-        return cropRepository.save(crop);
+        // Check if the Crop already exists in the database based on CropCategory ID and disease
+        List<Crop> existingCrops = cropRepository.findByCategoryTitleAndDiseaseExact(
+                crop.getCropCategory().getId(), crop.getDisease()
+        );
+
+        if (existingCrops.isEmpty()) {
+            // No Crop with the same CropCategory ID and disease found, so add the new Crop
+            return cropRepository.save(crop);
+        } else {
+            // A Crop with the same CropCategory ID and disease already exists
+            // You can choose to handle this situation as you desire, e.g., return an error message or update the existing Crop.
+            // For simplicity, let's just return null here.
+            return null;
+//            throw new IllegalArgumentException("Crop with the same CropCategory and disease already exists.");
+        }
     }
+
+
 
     @PutMapping("/{id}")
     public Crop updateCrop(@PathVariable Long id, @RequestBody Crop updatedCrop) {
--- src/main/java/com/javafest/aifarming/repository/CropRepository.java
+++ src/main/java/com/javafest/aifarming/repository/CropRepository.java
@@ -9,9 +9,18 @@ import java.util.List;
 
 @Repository
 public interface CropRepository extends JpaRepository<Crop, Long> {
-    @Query("SELECT c FROM Crop c JOIN FETCH c.cropCategory cc WHERE c.disease LIKE %?1% OR cc.title LIKE %?1%")
-    List<Crop> findByDisease(String keyword);
+    @Query("SELECT c FROM Crop c WHERE c.disease = ?1")
+    List<Crop> findByDisease(String disease);
 
     @Query("SELECT c FROM Crop c JOIN FETCH c.cropCategory cc WHERE cc.title = ?1 AND c.disease LIKE %?2%")
     List<Crop> findByCategoryTitleAndDisease(String categoryTitle, String disease);
+
+    @Query("SELECT c FROM Crop c JOIN FETCH c.cropCategory cc WHERE cc.id = ?1 AND c.disease = ?2")
+    List<Crop> findByCategoryTitleAndDiseaseExact(Long categoryId, String disease);
+
+    @Query("SELECT c FROM Crop c JOIN FETCH c.cropCategory cc WHERE cc.title = ?1")
+    List<Crop> findByTitle(String categoryTitle);
+
+    @Query("SELECT c FROM Crop c JOIN FETCH c.cropCategory cc WHERE c.disease LIKE %?1% OR cc.title LIKE %?1%")
+    List<Crop> findBySearch(String keyword);
 }
