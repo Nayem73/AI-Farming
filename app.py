4e77f432fb9ef2aaf343e23cb3e98f4e13271172
HSAkash <hemelakash472@gmail.com>
2023-07-29 10:51:30 +0600

    docker

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 
-from fastapi import FastAPI, File, UploadFile, Form
+from fastapi import FastAPI, File, UploadFile, Form, status, Response
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 import numpy as np
 from io import BytesIO
 from PIL import Image
-import tensorflow as tf
-from diseasesClassification.diseasesClassification import CropDiseaseML
+from ML_deseases.diseasesClassification import CropDiseaseML
+
+
+
 crop_disease_ml = CropDiseaseML()
 print(f"""
 _________________________HSAkash_________________________
@@ -30,30 +32,33 @@ app.add_middleware(
 )
 
 
-@app.get("/ping")
-async def ping():
-    return "Hello, I am alive"
-
 def read_file_as_image(data) -> np.ndarray:
     image = np.array(Image.open(BytesIO(data)))
     return image
 
-@app.post("/predict")
+@app.post("/predict", status_code=status.HTTP_200_OK)
 # multiple parameters
 
 async def predict(
-    file: UploadFile = File(...)
-    , crop: str = Form(...)
-):  
+    response: Response,file: UploadFile = File(...),  crop: str = Form(...)):  
     try:
         image = read_file_as_image(await file.read())
     except Exception as e:
+        response.status_code = status.HTTP_400_BAD_REQUEST
         return {
             'error': str(e)
         }
-    return {
-        'class': crop_disease_ml.predict(image, crop),
-    }
+    disease = crop_disease_ml.predict(image, crop)
+    print(disease)
+    if disease:
+        return {
+            'class': disease
+        }
+    else:
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return {
+            'error': 'Invalid crop'
+        }
 
 if __name__ == "__main__":
     uvicorn.run(app, host='0.0.0.0', port=8000)
--- build_files.sh
+++
@@ -1,2 +0,0 @@
-pip install -r requirements.txt
-pip install -i https://test.pypi.org/simple/ diseasesClassification==0.0.3
