23b2a32a6ff48999fe2a2552f88bc0d7f61ee29c
HSAkash <hemelakash472@gmail.com>
2023-08-04 19:50:38 +0600

    aiSearch user permission

--- Frontend/src/screens/AISearchScreen.js
+++ Frontend/src/screens/AISearchScreen.js
@@ -1,13 +1,23 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+
+
 import ImageUpload from '../components/ImageUpload.js';
 
-
+import React, {  useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { Link, useNavigate } from 'react-router-dom';
 
 
 const AISearchScreen = () => {
 
-    
+    const userLogin = useSelector(state => state.userLogin);
+    const { userInfo } = userLogin;
+    const history = useNavigate();
+
+    useEffect(() => {
+        if (!userInfo) {
+            history('/login')
+        }
+    }, [])
     
 
     return (
