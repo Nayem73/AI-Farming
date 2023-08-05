372c089a782b37efd8f705020508f2f890254184
HSAkash <hemelakash472@gmail.com>
2023-08-05 08:53:58 +0600

    card updated

--- Frontend/src/components/DiseaseCard.js
+++ Frontend/src/components/DiseaseCard.js
@@ -10,24 +10,22 @@ function DiseaseCard({ disease }) {
     return (
         <Card className="shadow-lg p-3 mb-5 bg-dark rounded-lg order-sm-1 mb-sm-0">
             <Link to={`/disease/${disease.crop.title}/${disease.title}`}>
-                <Card.Img className="shadow-sm mt-3" src={disease.img} />
-            
+                <div className="image-container">
+                    <Card.Img className="shadow-sm mt-3 object-fit" src={disease.img} />
+                </div>
 
-            <Card.Body>
-                <Link to={`/disease/${disease.crop.title}/${disease.title}`}>
+                <Card.Body>
                     <Card.Title as="div">
                         <strong>{disease.title}</strong>
                     </Card.Title>
-                </Link>
 
-
-                <Card.Text as="h3">
-                    {disease.crop.title}
-                </Card.Text>
-            </Card.Body>
+                    <Card.Text as="h3">
+                        {disease.crop.title}
+                    </Card.Text>
+                </Card.Body>
             </Link>
         </Card>
-    )
+    );
 }
 
 
