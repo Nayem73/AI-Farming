7d58767a44b81dbfc687da648d50264744435696
HSAkash <hemelakash472@gmail.com>
2023-08-05 08:00:26 +0600

    Footer updated

--- Frontend/src/components/Footer.js
+++ Frontend/src/components/Footer.js
@@ -6,10 +6,9 @@ function Footer() {
         <footer className="footer p-10 bg-base-200 text-base-content">
   <div>
     <span className="footer-title">Services</span> 
-    <a className="link link-hover">Branding</a> 
-    <a className="link link-hover">Design</a> 
-    <a className="link link-hover">Marketing</a> 
-    <a className="link link-hover">Advertisement</a>
+    <a className="link link-hover">Crop Details</a> 
+    <a className="link link-hover">Image Search</a> 
+    <a className="link link-hover">AI</a> 
   </div> 
   <div>
     <span className="footer-title">Company</span> 
