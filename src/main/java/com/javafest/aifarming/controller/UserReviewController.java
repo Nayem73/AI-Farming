363effc9b8d839819c7573dfa02d40ad0368594e
Nayem73 <nayem73@gmail.com>
2023-08-21 22:09:58 +0600

    pagination default page size 10

--- src/main/java/com/javafest/aifarming/controller/UserReviewController.java
+++ src/main/java/com/javafest/aifarming/controller/UserReviewController.java
@@ -36,7 +36,7 @@ public class UserReviewController {
     public ResponseEntity<Page<Map<String, Object>>> getAllUserReviewByUserId(
             @PathVariable Long userId,
             @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "5") int size) {
+            @RequestParam(defaultValue = "10") int size) {
 
         Pageable pageable = PageRequest.of(page, size);
 
