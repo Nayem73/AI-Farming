89aac56a0a0b5f1fecab2334ae17941bc3d3a955
HSAkash <hemelakash472@gmail.com>
2023-08-25 17:56:53 +0600

    update review card

--- Frontend/src/screens/ReviewScreen.js
+++ Frontend/src/screens/ReviewScreen.js
@@ -1,5 +1,5 @@
 import '../ReviewBox.css'; // Import your CSS file for styling
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Button, Card, Form } from 'react-bootstrap'
@@ -34,6 +34,7 @@ function ReviewScreen() {
     const [imageFile, setImageFile] = useState(null);
     const [usingCamera, setUsingCamera] = useState(false);
     const [reviewId, setReviewId] = useState(null);
+    const formRef = useRef(null);
 
     // create review object
     const [formData, setFormData] = useState({
@@ -71,7 +72,13 @@ function ReviewScreen() {
         setFormData({
             img: null,
             comment: review.comment
-        });}
+        });
+        formRef.current.scrollIntoView({
+            behavior: 'smooth', // You can adjust the scrolling behavior
+            block: 'start' // You can adjust the scroll position within the form
+        });
+    
+    }
 
     
 
@@ -104,61 +111,75 @@ function ReviewScreen() {
     };
 
     return <div>
-        <div className="mt-5 flex flex-col w-full mb-12 text-center">
+        <div className="mt-5 flex flex-col w-full text-center">
             <h1 className="mb-6 text-2xl font-semibold tracking-widest text-black uppercase title-font">Reviews</h1>
         </div>
-        <div className="container px-5 py-24 mx-auto">
-            <div className="flex flex-wrap -m-4">
+        <div className="container px-5 py-4 mx-auto">
+            <div className="-m-4">
                 {reviews.map((review) => (
-                    <div className="p-4 lg:w-1/3 md:w-1/2">
+                    <div className="p-4 review mt-2">
                         <div className="h-full px-8 py-10 border-2 border-gray-200 rounded-lg dark:border-gray-800">
-                            <div className="flex flex-col items-center mb-3">
-                                <div className="inline-flex items-center justify-center flex-shrink-0 w-20 h-20 mb-5 text-blue-500 bg-blue-100 rounded-full dark:bg-blue-500 dark:text-blue-100">
-                                    <svg className="w-10 h-10" fill="currentColor" viewBox="0 0 24 24">
-                                        <path
-                                            d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514
-                                            2 12 2zm0 18c-4.411 0-8-3.589-8-8s3.589-8
-                                            8-8 8 3.589 8 8-3.589 8-8 8z">
-                                        </path>
-                                        <path
-                                            d="M13 7h-2v5H8v2h3v5h2v-5h3v-2h-3z">
-                                        </path>
-                                    </svg>
+                            <div className="flex flex-col  mb-3">
+                                <div className="inline-flex items-center justify-center flex-shrink-0 w-15 h-20 mb-5 text-blue-500 bg-blue-100 rounded-full dark:bg-blue-500 dark:text-blue-100">
+                                
+                                    <i class="fa-regular fa-user fa-2xl px-8"></i>
+                                    <h2 className='px-8 font-bold text-xl title-font'>{review.userName}</h2>
                                 </div>
-                                <div className="flex-grow">
-                                    <h2 className="mb-3 text-lg font-medium text-gray-900 title-font">{review.userName}</h2>
-                                    <p className="text-sm text-gray-500">Created: {review.created}</p>
+
+
+
+                                {(review.img !== undefined && review.img !== null && review.img !== "" && review.img !== "null")
+                                ?<>
+                                <div className="flex-grow p-4 lg:w-1/3 md:w-1/2 card">
                                     <img src={review.img} alt={review.userName} fluid rounded />
-                                    <p className="text-base leading-relaxed">{review.comment.substring(0, 200)}</p>
-                                    {/* <p className="text-sm text-gray-500">Rating: {review.rating}</p> */}
+                                </div>
+                                <hr className="mb-5 mt-5" style={{color: '#000000'}}/>
+                                
+                                </>: <></>}
+    
+
+                                <div className="flex-grow  rounded-lg dark:border-gray-800 
+                                dark:bg-gray-100 p-8
+                                dark:hover:bg-gray-200 transition duration-500 ease-in-out
+                                
+                                ">
+                                    
+                                    <p className="text-base leading-relaxed">{review.comment}</p>
+                                    
                                 </div>
                             </div>
                             <div className="flex justify-center">
                                 <button className="inline-flex px-4 py-2 text-base font-semibold text-white transition duration-500 ease-in-out transform bg-blue-500 border-blue-500 rounded-lg hover:bg-blue-700 hover:border-blue-700 focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2" onClick={() => updateHandler(review.reviewId)}>Edit</button>
                                 <button className="inline-flex px-4 py-2 ml-4 text-base font-semibold text-white transition duration-500 ease-in-out transform bg-red-500 border-red-500 rounded-lg hover:bg-red-700 hover:border-red-700 focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2" onClick={() => deleteHandler(review.reviewId)}>Delete</button>
                             </div>
+                            <p className="text-sm text-gray-500">Created: {review.created}</p>
                         </div>
                     </div>
                 ))}
             </div>
         </div>
 
-        <div className="flex justify-center mb-5">
+        <div className="flex justify-center m-10">
             <Paginate pages={total_page} page={cur_page} dispatcher_action={listReviews}/>
         </div>
-
+        {/* hr tag with box shadow */}
+        <hr className="mb-5 mt-5" style={{color: '#000000'}}/>
         {/* create review form */}
         
-        <div className="flex justify-center mb-5">
-            {errorReviewCreate && <Message message={errorReviewCreate} />}
-            {successReviewCreate && <SuccessMessage message={"Review is added successfully"} />}
-            
-            {errorReviewUpdate && <Message message={errorReviewUpdate} />}
-            {successReviewUpdate && <SuccessMessage message={"Review is updated"} />}
+        <div className="flex justify-center mb-5 mt-5">
+            <h1 className="mb-6 text-2xl font-semibold tracking-widest text-black uppercase title-font">Create or Update Reviews</h1>
+        </div>
+        <div className="flex justify-center mb-5 mt-5">
+                {errorReviewCreate && <Message message={errorReviewCreate} />}
+                {successReviewCreate && <SuccessMessage message={"Review is added successfully"} />}
+                
+                {errorReviewUpdate && <Message message={errorReviewUpdate} />}
+                {successReviewUpdate && <SuccessMessage message={"Review is updated"} />}
+
         </div>
         
-        <div className='lg:px-20 mt-10 mr-5 ml-5'>
-            <form id="myForm" onSubmit={handleSubmit}>
+        <div className='lg:px-20 mt-10 mr-5 ml-5 mb-10'>
+            <form ref={formRef}  id="myForm" onSubmit={handleSubmit}>
             {imageFile && !usingCamera && (
             <div className="container-aisearch">
                 <div className="ai_img">
