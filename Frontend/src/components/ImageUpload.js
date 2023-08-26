c733e69778a9b48eacd8a466a7b84c8ed6d9147b
HSAkash <hemelakash472@gmail.com>
2023-08-26 10:06:18 +0600

    add drag and drop

--- Frontend/src/components/ImageUpload.js
+++ Frontend/src/components/ImageUpload.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Select from 'react-select';
 import { listCrops } from '../actions/cropActions.js';
@@ -7,6 +7,7 @@ import { useNavigate } from 'react-router-dom'
 import {AI_SEARCH_RESET} from '../constants/diseaseConstants.js'
 import Message from '../components/Message';
 import Loader from '../components/Loader';
+import  {useDropzone} from 'react-dropzone';
 
 const ImageUpload = () => {
     const dispatch = useDispatch();
@@ -60,19 +61,8 @@ const ImageUpload = () => {
     });
 
     const [imageFile, setImageFile] = useState(null);
-    const [usingCamera, setUsingCamera] = useState(false);
 
-    const handleImageChange = (e) => {
-        const file = e.target.files[0];
-        setFormData((prevFormData) => ({
-        ...prevFormData,
-        file: file,
-        }));
-        if (file) {
-        setImageFile(file);
-        setUsingCamera(false);
-        }
-    };
+
 
     const handleCropChange = (selectedOption) => {
         setFormData((prevFormData) => ({
@@ -89,50 +79,77 @@ const ImageUpload = () => {
         dispatch(aiSearch(formDataToSend));
     };
 
-    return (
-        <>
-        {/* {message && <SuccessMessage message={message} />} */}
-            {message && <Message message={message} />}
-            {aiLoading && <Loader />}
-        <form onSubmit={handleSubmit}>
-            <div>
+// ____________________________drug_drop_down_______________________________________//
+// drag state
+
+
+const {getRootProps, getInputProps, isDragActive} = useDropzone({
+    accept: 'image/*',
+    onDrop: acceptedFiles => {
+        setFormData((prevFormData) => ({
+            ...prevFormData,
+            file: acceptedFiles[0],
+            }));
+            console.log(acceptedFiles[0])
+            if (acceptedFiles[0]) {
+            setImageFile(acceptedFiles[0]);
+            }
+    }
+})
+
+
+// ____________________________drug_drop_down_______________________________________//
+
+
+
+
+
+
+
+return (
+    <form  onSubmit={handleSubmit}>
+        {aiError && <Message message={aiError} />}
+        <div className="container px-5 py-24 mx-auto  border-2 border-gray-200 rounded-lg dark:border-gray-800 ">
+            <div className="flex flex-wrap -m-4">
+                <div className="mx-5 my-3 lg:w-1/3 md:w-1/2">
+                <div className="h-full border-2 border-gray-200 rounded-lg dark:border-gray-100">
+                    <label id="label-file-upload" htmlFor="input-file-upload" className={isDragActive ? "drag-active" : "" }>
+                        <div {...getRootProps()}>
+                            <input {...getInputProps()} />
+                            {
+                                isDragActive ?<p>Drop the image</p>:<p>Drag the image || click here</p>
+                            }
+                         
+                        </div> 
+                        
+                    </label>
+                </div> 
+                </div> 
+                <div className="mx-5 my-3 lg:w-1/3 md:w-1/2">
+                <div className="h-full  border-2 border-gray-200 rounded-lg dark:border-gray-800">
+                    {imageFile && 
+                        <img src={imageFile && URL.createObjectURL(imageFile)} alt="preview" className="preview-image" />
+                    }
+                </div>
+                </div>
+                    
+
+            </div>
+        </div>
+        <div>
             <label htmlFor="crop">Crop ID:</label>
             <Select
                 options={cropOptions}
                 onChange={handleCropChange}
                 value={cropOptions.find((option) => option.value === formData.crop)}
             />
-            </div>
-            <div>
-            <label htmlFor="file">Image:</label>
-            <input
-                type="file"
-                id="img"
-                name="img"
-                accept=".png, .jpg, .jpeg"
-                onChange={handleImageChange}
-            />
-            </div>
-
-            {imageFile && !usingCamera && (
-            <div className="container-aisearch">
-                <div className="ai_img">
-                <img
-                    src={URL.createObjectURL(imageFile)}
-                    alt="Uploaded"
-                    className="uploaded-image"
-                />
-                </div>
-            </div>
-            )}
-
-            <div className="py-4 flex justify-left">
+        </div>
+        <div className="py-4 flex justify-left">
             <button type="submit" className="btn btn-primary w-24">
                 Submit
             </button>
             </div>
-        </form>
-        </>
+    </form>
     );
 };
 
