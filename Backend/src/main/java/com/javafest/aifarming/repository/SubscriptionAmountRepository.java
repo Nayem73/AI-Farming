3c0e4c3c7c08f71267a921d6aaf912233ed9456c
Nayem73 <nayem73@gmail.com>
2023-08-29 09:23:11 +0600

    Alhamdulillah All 4 of SubscriptionAmount completed: SubscriptionAmount, SubscriptionAmountController, SubscriptionAmountRepository, SubscriptionAmountService

---
+++ Backend/src/main/java/com/javafest/aifarming/repository/SubscriptionAmountRepository.java
@@ -0,0 +1,7 @@
+package com.javafest.aifarming.repository;
+
+import com.javafest.aifarming.model.SubscriptionAmount;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubscriptionAmountRepository extends JpaRepository<SubscriptionAmount, Long> {
+}
