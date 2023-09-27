48416d9a506abd5fc2aa9392d6c3a09eb90975b7
Nayem73 <nayem73@gmail.com>
2023-09-27 22:08:18 +0600

    need to merge

--- Backend/src/main/java/com/javafest/aifarming/repository/SearchCountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.javafest.aifarming.repository;
-
-import com.javafest.aifarming.model.SearchCount;
-import com.javafest.aifarming.model.UserInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SearchCountRepository extends JpaRepository<SearchCount, Long> {
-    SearchCount findByUserInfo(UserInfo userInfo);
-}
