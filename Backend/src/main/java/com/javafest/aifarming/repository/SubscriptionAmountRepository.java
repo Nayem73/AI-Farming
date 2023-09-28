b59c25d57cfc24005cb6407fccc60ec59752078a
Nayem73 <nayem73@gmail.com>
2023-09-28 22:02:31 +0600

    merging backend from it's developing branch Spring-Boot-Backend

--- Backend/src/main/java/com/javafest/aifarming/repository/SubscriptionAmountRepository.java
+++
@@ -1,7 +0,0 @@
-package com.javafest.aifarming.repository;
-
-import com.javafest.aifarming.model.SubscriptionAmount;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubscriptionAmountRepository extends JpaRepository<SubscriptionAmount, Long> {
-}
